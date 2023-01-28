4a7779bb127d9dc49d5e39f6b4fcf37feeea871e
sian.me <sian.me@kakaoenterprise.com>
2023-01-28 13:19:49 +0900

    fix: eslint 재설치

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import styled from '@emotion/styled';
 const ExampleComponentCSS = styled.div`
   color: red;
 `;
+
 export default function Home() {
-  return <ExampleComponentCSS>emotion test</ExampleComponentCSS>;
+  return (<ExampleComponentCSS>emotion test</ExampleComponentCSS>);
 }
