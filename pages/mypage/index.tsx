f6dd500bd793ad6c9d67ce1363198f0d86af794d
sian.me <sian.me@kakaoenterprise.com>
2023-01-28 23:01:32 +0900

    feat: chakra 세팅

--- pages/mypage/index.tsx
+++ pages/mypage/index.tsx
@@ -1,7 +1,16 @@
+import PageHeader from '@/components/layouts/Header';
 import React from 'react';
 
 const MyPage = () => {
-  return <div>MyPage</div>;
+  return (
+    <>
+      <PageHeader />
+      <h1>My Page</h1>
+      <section>
+        <h2>마이페이지</h2>
+      </section>
+    </>
+  );
 };
 
 export default MyPage;
---
+++ styles/theme/foundations/fonts.ts
@@ -0,0 +1,3 @@
+export const fonts = {
+    fontFamily: 'Roboto, sans-serif',
+}
