f817844ab19b3a3d799d93086c7e726392210e4e
sian.me <sian.me@kakaoenterprise.com>
2023-01-28 23:03:13 +0900

    style: chakra global 스타일 추가

---
+++ styles/theme/styles.ts
@@ -0,0 +1,24 @@
+const styles = {
+  global: {
+    '*': {
+      boxSizing: 'border-box',
+    },
+    'html, body': {
+      margin: 0,
+      padding: 0,
+      height: '100%',
+    },
+    a: {
+      color: 'inherit',
+      textDecoration: 'none',
+    },
+    'textarea: focus, input: focus, button: focus':{
+      outline: 'none',
+    },
+    'ul': {
+      listStyle: 'none',
+    }
+  },
+};
+
+export default styles;
