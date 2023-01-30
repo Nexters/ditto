e22fb5fb681130cc7f649624b706bbb312f4ce19
young-do <yd199323@gmail.com>
2023-01-30 09:22:55 +0900

    use maxAge

--- pages/api/auth/kakao-logout.ts
+++ pages/api/auth/kakao-logout.ts
@@ -5,8 +5,8 @@ export default createHandler({
   GET: async (req, res) => {
     // clear cookie
     setCookie(res, [
-      { name: 'access_token', value: '', options: { path: '/', expires: new Date(1) } },
-      { name: 'refresh_token', value: '', options: { path: '/', expires: new Date(1) } },
+      { name: 'access_token', value: '', options: { path: '/', maxAge: 0 } },
+      { name: 'refresh_token', value: '', options: { path: '/', maxAge: 0 } },
     ]);
     res.status(307).redirect('/');
   },
