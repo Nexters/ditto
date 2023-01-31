28548e59fc816585991e89d36793bfe9eedf2de6
young-do <yd199323@gmail.com>
2023-01-31 16:39:48 +0900

    change max age to expires for cookie expiration

--- pages/api/auth/kakao-logout.ts
+++ pages/api/auth/kakao-logout.ts
@@ -9,8 +9,8 @@ const edgeFunction: EdgeFunction = () => {
   // @todo: 리다이렉트 경로를 환경변수로 설정
   const res = NextResponse.redirect('http://localhost:3000/', 307);
 
-  res.cookies.set('refresh_token', '', { path: '/', maxAge: 0, httpOnly: true, secure: true });
-  res.cookies.set('access_token', '', { path: '/', maxAge: 0, httpOnly: true, secure: true });
+  res.cookies.set('refresh_token', '', { path: '/', expires: new Date(1), httpOnly: true, secure: true });
+  res.cookies.set('access_token', '', { path: '/', expires: new Date(1), httpOnly: true, secure: true });
 
   return res;
 };
