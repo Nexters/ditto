709eafa32e3228b9d355f6ee2c0ba6f4402bc2f7
sangbooom <tkdqja4164@naver.com>
2023-02-02 00:11:39 +0900

    feat: ErrorBoundary

---
+++ components/ErrorBoundary/ErrorBoundary.ts
@@ -0,0 +1,34 @@
+import React, { Component, ErrorInfo, ReactNode } from 'react';
+
+interface Props {
+  children: ReactNode;
+  fallback?: React.ReactElement | null;
+}
+
+interface State {
+  hasError: boolean;
+}
+
+class ErrorBoundary extends Component<Props, State> {
+  public state: State = {
+    hasError: false,
+  };
+
+  public static getDerivedStateFromError(): State {
+    return { hasError: true };
+  }
+
+  public componentDidCatch(error: Error, errorInfo: ErrorInfo) {
+    console.error('Uncaught error:', error, errorInfo);
+  }
+
+  public render() {
+    if (this.state.hasError) {
+      return this.props.fallback || null;
+    }
+
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
