1990a6eaa93d0b29bda7ec4b6c667458bb88f590
young-do <yd199323@gmail.com>
2023-02-07 22:26:38 +0900

    set login url to const

--- utils/const.ts
+++ utils/const.ts
@@ -1,3 +1,5 @@
+import queryString from 'query-string';
+
 export const HOSTING_URL = process.env.NEXT_PUBLIC_HOSTING_URL || 'http://localhost:3000';
 
 export const KAKAO_CLIENT_ID = process.env.NEXT_PUBLIC_KAKAO_REST_API_KEY as string;
@@ -6,6 +8,23 @@ export const KAKAO_LOGIN_REDIRECT_URL =
   process.env.NEXT_PUBLIC_KAKAO_LOGIN_REDIRECT_URL || 'http://localhost:3000/api/auth/kakao-login';
 export const KAKAO_LOGOUT_REDIRECT_URL =
   process.env.NEXT_PUBLIC_KAKAO_LOGOUT_REDIRECT_URL || 'http://localhost:3000/api/auth/kakao-logout';
+
+export const KAKAO_LOGIN_URL = queryString.stringifyUrl({
+  url: 'https://kauth.kakao.com/oauth/authorize',
+  query: {
+    client_id: KAKAO_CLIENT_ID,
+    redirect_uri: KAKAO_LOGIN_REDIRECT_URL,
+    response_type: 'code',
+  },
+});
+export const KAKAO_LOGOUT_URL = queryString.stringifyUrl({
+  url: 'https://kauth.kakao.com/oauth/logout',
+  query: {
+    client_id: KAKAO_CLIENT_ID,
+    logout_redirect_uri: KAKAO_LOGOUT_REDIRECT_URL,
+    response_type: 'code',
+  },
+});
 
 export const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
 export const SUPABASE_KEY = process.env.NEXT_PUBLIC_SUPABASE_KEY as string;
