8a3e73594d4af7d1f81c7bb0fd948b8c9dad5dae
young-do <yd199323@gmail.com>
2023-02-07 19:15:04 +0900

    로그인 loading 상태값 추가

--- store/useUser.ts
+++ store/useUser.ts
@@ -11,6 +11,7 @@ type UserState = (
       isLoggedIn: false;
     }
 ) & {
+  isLoading: boolean;
   login: () => Promise<void>;
   logout: () => Promise<void>;
 };
@@ -18,12 +19,19 @@ type UserState = (
 export const useUser = create<UserState>((set) => ({
   user: null,
   isLoggedIn: false,
+  isLoading: false,
   login: async () => {
     // @note: 세션에 담긴 쿠키를 통해 자신의 유저 정보를 가져옵니다.
-    const res = await fetch('/api/auth/me');
-    const user = (await res.json())?.data?.user;
-    if (!user) throw 'empty user info';
-    set({ user, isLoggedIn: true });
+    set({ isLoading: true });
+    try {
+      const res = await fetch('/api/auth/me');
+      const user = (await res.json())?.data?.user;
+      if (!user) throw 'empty user info';
+      set({ user, isLoggedIn: true, isLoading: false });
+    } catch (error) {
+      set({ isLoading: false });
+      throw error;
+    }
   },
   logout: async () => {
     // @note: 카카오 로그아웃 url 방문없이, 세션에 담긴 쿠키를 제거합니다.
