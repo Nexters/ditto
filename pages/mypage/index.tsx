aea92a6d99c9a03fe5d37203aa63fe171fdeba1c
young-do <yd199323@gmail.com>
2023-02-08 00:05:07 +0900

    add logout button

--- pages/mypage/index.tsx
+++ pages/mypage/index.tsx
@@ -2,13 +2,24 @@ import PageHeader from '@/components/layouts/Header';
 import MainLayout from '@/components/layouts/MainLayout';
 import React from 'react';
 import { NextPageWithLayout } from '@/pages/_app';
+import { useUser } from '@/store/useUser';
+import { useRouter } from 'next/router';
 
 const MyPage: NextPageWithLayout = () => {
+  const router = useRouter();
+  const { logout } = useUser();
+
+  const handleLogout = async () => {
+    await logout();
+    router.push('/');
+  };
+
   return (
     <>
       <h1>My Page</h1>
       <section>
         <h2>마이페이지</h2>
+        <button onClick={handleLogout}>로그아웃</button>
       </section>
     </>
   );
