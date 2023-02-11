38abbf5fa665ea07827045086c0019f16121dd35
siyeon <siyeonleeme@gmail.com>
2023-02-12 06:36:08 +0900

    fix: base modal props 수정

--- components/modals/BaseModal.tsx
+++ components/modals/BaseModal.tsx
@@ -5,18 +5,26 @@ interface BaseModalProps {
   isOpen: boolean;
   onClose: () => void;
   modalContent: ReactNode;
+  height?: number;
+  width?: number;
 }
 
 const BaseModal = (props: BaseModalProps) => {
-  const { isOpen, onClose, modalContent } = props;
+  const { isOpen, onClose, modalContent, width, height } = props;
   return (
     <Modal isOpen={isOpen} onClose={onClose}>
-      <ModalOverlay bg="blackAlpha.100" />
-      <ModalContent bgColor="white" width={300} height={200}>
+      <ModalOverlay bg="blackAlpha.300" />
+      <ModalContent style={ModalStyle} width={width ? width : 300} height={height ? height : 230}>
         {modalContent}
       </ModalContent>
     </Modal>
   );
+};
+
+const ModalStyle = {
+  backgroundColor: 'white',
+  boxShadow: 'none',
+  marginTop: 138,
 };
 
 export default BaseModal;
