2a77d81625592f553c82480e2a4bc26071702beb
sian.me <sian.me@kakaoenterprise.com>
2023-02-11 16:04:53 +0900

    feat: icon 에셋 컴포넌트화

---
+++ components/icons/index.ts
@@ -0,0 +1,7 @@
+export { default as Calendar } from './Calendar';
+export { default as ErrorRabbit } from './ErrorRabbit';
+export { default as Flag } from './Flag';
+export { default as MainLogo } from './MainLogo';
+export { default as RabbitWithCar } from './RabbitWithCar';
+export { default as SplashLogo } from './SplashLogo';
+export { default as TrashCan } from './TrashCan';
