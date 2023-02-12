e2127da8c814107eac33de1c16563de1a31b128a
young-do <yd199323@gmail.com>
2023-02-12 17:11:22 +0900

    add styling by error template

--- pages/no-group.tsx
+++ pages/no-group.tsx
@@ -1,5 +1,8 @@
+import { ErrorTemplate } from '@/components/errors/ErrorTemplate';
 import MainLayout from '@/components/layouts/MainLayout';
 import { KAKAO_LOGOUT_URL } from '@/utils/const';
+import { Button } from '@chakra-ui/button';
+import styled from '@emotion/styled';
 
 // 필요한 기능
 // - 카카오 로그아웃
@@ -7,13 +10,37 @@ import { KAKAO_LOGOUT_URL } from '@/utils/const';
 const NoGroupPage = () => {
   return (
     <MainLayout hideBottomNavigation>
-      <h1>참여한 그룹이 없습니다.</h1>
-      <p>
-        새 그룹을 생성하고 싶다면 <u>서비스 관리자에게 문의</u>해주세요.
-      </p>
-      <a href={KAKAO_LOGOUT_URL}>카카오 계정 로그아웃</a>
+      <ErrorTemplate
+        title={'참여한 그룹이 없습니다.'}
+        description={
+          <>
+            새 그룹을 생성하고 싶다면
+            <br />
+            <UnderlinedText>서비스 관리자에게 문의</UnderlinedText>해주세요.
+          </>
+        }
+      >
+        <Button
+          as={'a'}
+          href={KAKAO_LOGOUT_URL}
+          padding={'17px 24px'}
+          height={'50px'}
+          backgroundColor={'#1A1919'}
+          color={'white'}
+          fontSize={'16px'}
+          lineHeight={'1'}
+          borderRadius={'25px'}
+        >
+          {'카카오 계정 로그아웃'}
+        </Button>
+      </ErrorTemplate>
     </MainLayout>
   );
 };
+
+const UnderlinedText = styled.a`
+  color: #0080ff;
+  text-decoration: underline;
+`;
 
 export default NoGroupPage;
