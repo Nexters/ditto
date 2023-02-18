91d4bcafc9324116dc27e940c3fc03b7f24b34a6
young-do <yd199323@gmail.com>
2023-02-19 00:53:16 +0900

    change header not scrolled

--- pages/mypage/index.tsx
+++ pages/mypage/index.tsx
@@ -9,14 +9,17 @@ const MyPage: NextPageWithLayout = () => {
 
   return (
     <MainLayout>
-      <MyPageHeader>My Page</MyPageHeader>
-      <section>
+      <MyPageHeader>
+        My Page
+        <div>dropdown</div>
+      </MyPageHeader>
+      <MyPageContent>
         <MemberList />
         <MyPageDivider />
         <OtherButton>새 그룹 만들기</OtherButton>
         <OtherButton>문의하기</OtherButton>
         <OtherButton onClick={logout}>로그아웃</OtherButton>
-      </section>
+      </MyPageContent>
     </MainLayout>
   );
 };
@@ -26,15 +29,28 @@ MyPage.isProtectedPage = true;
 export default MyPage;
 
 const MyPageHeader = styled.h1`
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+
   display: flex;
   justify-content: space-between;
   align-items: center;
+
   height: 58px;
   padding: 0 20px;
   ${(props) => props.theme.textStyles.h3}
   border-bottom: 1px solid ${(props) => props.theme.colors.grey['2']};
 `;
-
+const MyPageContent = styled.section`
+  position: absolute;
+  top: 58px;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  overflow: auto;
+`;
 const MyPageDivider = styled.div`
   width: 100%;
   height: 6px;
