ca808aa0576fcba770c7adbf0951d1bdbe90ffeb
young-do <yd199323@gmail.com>
2023-02-19 00:46:59 +0900

    markup mypage

---
+++ components/memberList/MemberList.tsx
@@ -0,0 +1,40 @@
+import { useFetchMemberList } from '@/hooks/useFetchMemberList';
+import { useUser } from '@/store/useUser';
+import { Button } from '@chakra-ui/button';
+import styled from '@emotion/styled';
+import { MemberItem } from './MemberItem';
+
+export const MemberList = () => {
+  const { user, selectedGroupId } = useUser();
+  const { data } = useFetchMemberList(user, selectedGroupId);
+
+  if (!user) return null;
+  return (
+    <MemberListWrap>
+      <MemberListHeader>멤버 리스트</MemberListHeader>
+      <MemberItem key={user.id} nickname={user.nickname} profileImage={user.profile_image} isMe />
+      {data?.map((member) => (
+        <MemberItem key={member.id} nickname={member.nickname} profileImage={member.profile_image} />
+      ))}
+      <AddMemberButton>멤버 추가하기</AddMemberButton>
+    </MemberListWrap>
+  );
+};
+
+const MemberListWrap = styled.section`
+  padding: 0 20px;
+  border-bottom: 1px solid ${(props) => props.theme.colors.grey[2]};
+`;
+const MemberListHeader = styled.h3`
+  padding: 12px 0;
+  ${(props) => props.theme.textStyles.h3}
+`;
+const AddMemberButton = styled(Button)`
+  width: 100%;
+  height: auto;
+  margin: 20px 0;
+  padding: 16px 0;
+  ${(props) => props.theme.textStyles.buttonSmall}
+  color: ${(props) => props.theme.colors.grey[1]};
+  background-color: ${(props) => props.theme.colors.grey[10]};
+`;
--- pages/mypage/index.tsx
+++ pages/mypage/index.tsx
@@ -1,32 +1,51 @@
-import PageHeader from '@/components/layouts/Header';
 import MainLayout from '@/components/layouts/MainLayout';
+import { MemberList } from '@/components/memberList/MemberList';
 import { NextPageWithLayout } from '@/pages/_app';
 import { useUser } from '@/store/useUser';
-import Image from 'next/image';
+import styled from '@emotion/styled';
 
 const MyPage: NextPageWithLayout = () => {
-  const { user, logout } = useUser();
+  const { logout } = useUser();
 
   return (
-    <>
-      <h1>My Page</h1>
+    <MainLayout>
+      <MyPageHeader>My Page</MyPageHeader>
       <section>
-        <h2>마이페이지</h2>
-        <Image src={user?.profile_image || ''} alt="profile image" width={60} height={60} />
-        <h3>{user?.nickname}</h3>
-        <button onClick={logout}>로그아웃</button>
+        <MemberList />
+        <MyPageDivider />
+        <OtherButton>새 그룹 만들기</OtherButton>
+        <OtherButton>문의하기</OtherButton>
+        <OtherButton onClick={logout}>로그아웃</OtherButton>
       </section>
-    </>
+    </MainLayout>
   );
 };
-
-MyPage.getLayout = (page) => (
-  <MainLayout>
-    <PageHeader />
-    {page}
-  </MainLayout>
-);
 
 MyPage.isProtectedPage = true;
 
 export default MyPage;
+
+const MyPageHeader = styled.h1`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 58px;
+  padding: 0 20px;
+  ${(props) => props.theme.textStyles.h3}
+  border-bottom: 1px solid ${(props) => props.theme.colors.grey['2']};
+`;
+
+const MyPageDivider = styled.div`
+  width: 100%;
+  height: 6px;
+  background-color: #f3f5f5;
+`;
+
+const OtherButton = styled.button`
+  width: 100%;
+  padding: 22px 20px;
+  text-align: left;
+  border-bottom: 1px solid ${(props) => props.theme.colors.grey[2]};
+  ${(props) => props.theme.textStyles.buttonMedium}
+  color: ${(props) => props.theme.colors.grey[10]};
+`;
