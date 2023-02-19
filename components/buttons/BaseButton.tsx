c39a7e017596a0fa8cafffe7df53d08cc96c4c61
siyeon <siyeonleeme@gmail.com>
2023-02-19 17:12:54 +0900

    fix: base button 클릭이벤트 추가

--- components/buttons/BaseButton.tsx
+++ components/buttons/BaseButton.tsx
@@ -3,15 +3,17 @@ import { Button } from '@chakra-ui/react';
 interface BaseButtonProps {
   isDisabled?: boolean;
   label?: string;
+  onClick?: () => void;
 }
 
 const BaseButton = (props: BaseButtonProps) => {
-  const { isDisabled, label } = props;
+  const { isDisabled, label, onClick } = props;
   return (
     <Button
       bgColor={isDisabled ? 'disabled' : 'secondary'}
       color={'white'}
       _hover={{ bgColor: isDisabled ? 'disabled' : 'secondary' }}
+      onClick={onClick}
     >
       {label}
     </Button>
