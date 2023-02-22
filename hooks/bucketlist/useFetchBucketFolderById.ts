726580765c7d749e7c5ba73eafef5c7801657378
sian.me <sian.me@kakaoenterprise.com>
2023-02-23 02:32:57 +0900

    feat: bucket 상세페이지 타이틀 추가, 상세 API 추가

---
+++ hooks/bucketlist/useFetchBucketFolderById.ts
@@ -0,0 +1,13 @@
+import { useQuery } from '@tanstack/react-query';
+import { BucketFolder } from '@/lib/supabase/type';
+import { getBucketFolderById } from '@/lib/supabase/apis/bucketlist';
+
+export const useFetchBucketFolderById = (id: number) => {
+  const fetcher = async () => {
+    const response = await getBucketFolderById(id);
+    return response;
+  };
+  return useQuery<BucketFolder, Error>(['bucketFolder'], fetcher, {
+    staleTime: Infinity,
+  });
+};
