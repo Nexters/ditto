3b528e014e38619e5a3c35608dd915fa828cd08b
young-do <yd199323@gmail.com>
2023-02-25 00:31:46 +0900

    그룹 메뉴 마크업

--- components/groupMenu/GroupMenu.tsx
+++ components/groupMenu/GroupMenu.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { useFetchJoinedGroupList } from '@/hooks/group/useFetchJoinedGroupList';
 import { useUser } from '@/store/useUser';
-import { Menu, MenuButton, MenuList, MenuItem, Button } from '@chakra-ui/react';
-import { CheckIcon20, GrayDownIcon } from '../icons';
+import { Menu, MenuButton, MenuList, MenuItem, forwardRef } from '@chakra-ui/react';
+import { GrayDownIcon, PawIcon } from '../icons';
+import styled from '@emotion/styled';
 
 const GroupMenu = () => {
   const { user, selectedGroupId, setGroupId } = useUser();
@@ -10,21 +11,67 @@ const GroupMenu = () => {
 
   const currentGroupName = groupList?.find(({ id }) => id === selectedGroupId)?.name || '';
 
+  const ForwardedCustomMenuButton = forwardRef((props, ref) => (
+    <CustomMenuButton ref={ref} {...props}>
+      {currentGroupName}
+      <GrayDownIcon />
+    </CustomMenuButton>
+  ));
+
   return (
     <Menu>
-      <MenuButton as={Button} rightIcon={<GrayDownIcon />}>
-        {currentGroupName}
-      </MenuButton>
-      <MenuList>
-        {groupList?.map(({ name, id }) => (
-          <MenuItem key={id} onClick={() => setGroupId(id)}>
-            {id === selectedGroupId ? <CheckIcon20 /> : null}
-            {name}
-          </MenuItem>
-        ))}
-      </MenuList>
+      <MenuButton as={ForwardedCustomMenuButton} />
+      <CustomMenuList>
+        {groupList?.map(({ name, id }) => {
+          const selected = id === selectedGroupId;
+          return (
+            <CustomMenuItem key={id} onClick={() => setGroupId(id)} selected={selected}>
+              {name}
+              {selected && <PawIcon width={18} height={18} />}
+            </CustomMenuItem>
+          );
+        })}
+      </CustomMenuList>
     </Menu>
   );
 };
 
 export default GroupMenu;
+
+const CustomMenuButton = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 4px;
+
+  ${(props) => props.theme.textStyles.buttonSmall}
+  color: ${(props) => props.theme.colors.grey[6]};
+`;
+const CustomMenuList = styled(MenuList)`
+  width: 160px;
+  min-width: 160px;
+  padding: 0;
+  margin-top: -4px;
+  border-radius: 8px;
+  background-color: ${(props) => props.theme.colors.white};
+  overflow: hidden;
+`;
+const CustomMenuItem = styled(MenuItem)<{ selected: boolean }>`
+  padding: 12px;
+  justify-content: space-between;
+  ${(props) => props.theme.textStyles.buttonSmall}
+  ${(props) =>
+    props.selected
+      ? `color: ${props.theme.colors.white};
+        background-color: ${props.theme.colors.grey[10]};`
+      : `color: ${props.theme.colors.grey[6]};`};
+
+  svg {
+    filter: invert(1);
+  }
+
+  &:focus,
+  &:active {
+    --menu-bg: none;
+  }
+`;
