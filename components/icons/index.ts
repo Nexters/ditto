d606487d88acad645753611fa3e9ff8af1b1a55d
sangbooom <tkdqja4164@naver.com>
2023-02-25 01:17:49 +0900

    feat: closeIcon 추가

--- components/icons/index.ts
+++ components/icons/index.ts
@@ -1,9 +1,9 @@
-export { default as ArrowRightIcon } from './ArrowRightIcon';
 export { default as BlackDownIcon } from './BlackDownIcon';
 export { default as CalendarIcon } from './CalendarIcon';
 export { default as CheckIcon20 } from './CheckIcon20';
 export { default as CheckIcon24 } from './CheckIcon24';
 export { default as CheckIcon32 } from './CheckIcon32';
+export { default as CloseIcon } from './CloseIcon';
 export { default as ErrorRabbitIcon } from './ErrorRabbitIcon';
 export { default as FlagIcon } from './FlagIcon';
 export { default as FolderIcon } from './FolderIcon';
