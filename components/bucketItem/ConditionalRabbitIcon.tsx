c48832d6ef46222ae4a6b9f6440ef2a013ee4f3f
siyeon <siyeonleeme@gmail.com>
2023-02-25 12:23:51 +0900

    feat: conditional rabbit icon 컴포넌트 추가

---
+++ components/bucketItem/ConditionalRabbitIcon.tsx
@@ -0,0 +1,30 @@
+import { getRandomColor } from '@/utils/getRandomColor';
+import { RabbitWithCakeIcon, RabbitWithCarIcon, RandomRabbitIcon } from '@/components/icons';
+import React, { useMemo } from 'react';
+import { Flex } from '@chakra-ui/react';
+import PartialLoader from '@/components/loading/PartialLoader';
+
+const PlaceKeywordList = ['여행', '여행지', '장소', '곳'];
+const FoodKeywordList = ['음식', '맛집', '먹을거리', '카페', '음식점', '먹을거리'];
+
+const ConditionalRabbitIcon = ({ folderTitle }: { folderTitle: string }) => {
+  const isPlace = useMemo(() => {
+    return PlaceKeywordList.some((keyword) => folderTitle.includes(keyword));
+  }, [folderTitle]);
+
+  const isFood = useMemo(() => {
+    return FoodKeywordList.some((keyword) => folderTitle.includes(keyword));
+  }, [folderTitle]);
+
+  if (!folderTitle) return <PartialLoader />;
+
+  return (
+    <Flex justifyContent={'flex-end'}>
+      {isPlace && <RabbitWithCarIcon />}
+      {isFood && <RabbitWithCakeIcon />}
+      {!isPlace && !isFood && <RandomRabbitIcon rabbitColor={getRandomColor()} />}
+    </Flex>
+  );
+};
+
+export default ConditionalRabbitIcon;
