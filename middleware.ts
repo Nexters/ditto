18fe2c521598b3102fab330e85a1be37fcdad24e
young-do <yd199323@gmail.com>
2023-02-26 23:32:27 +0900

    add request

--- middleware.ts
+++ middleware.ts
@@ -1,14 +1,14 @@
 // middleware.ts
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 
 // This function can be marked `async` if using `await` inside
-export function middleware() {
-  const res = NextResponse.next();
-  res.headers.append('x-matched-path', '/bucketlist/[folderId]');
+export function middleware(request: NextRequest) {
+  const res = NextResponse.next({ request });
+  res.headers.append('x-matched-path', '/testing/[id]');
   return res;
 }
 
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: ['/bucketlist/:path*', '/testing/:path*'],
+  matcher: ['/testing/:path*'],
 };
