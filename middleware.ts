7953be0ae6bc2933e624c603fe3bf8724875fba3
young-do <yd199323@gmail.com>
2023-02-26 23:16:18 +0900

    update for testing

--- middleware.ts
+++ middleware.ts
@@ -10,5 +10,5 @@ export function middleware() {
 
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: '/bucketlist/:path*',
+  matcher: ['/bucketlist/:path*', '/testing/:path*'],
 };
