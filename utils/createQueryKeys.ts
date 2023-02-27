d3ebf171dc2e2ecfa4ef2132956a6da0bf48c6c4
young-do <yd199323@gmail.com>
2023-02-27 23:11:11 +0900

    use unknown type

--- utils/createQueryKeys.ts
+++ utils/createQueryKeys.ts
@@ -3,9 +3,9 @@ export const createQueryKeys = (keyword: string) => {
   const keys = {
     all: [keyword] as const,
     lists: () => [...keys.all, 'list'] as const,
-    list: (args: any[]) => [...keys.lists(), ...args] as const,
+    list: (args: unknown[]) => [...keys.lists(), ...args] as const,
     details: () => [...keys.all, 'detail'] as const,
-    detail: (args: any[]) => [...keys.details(), ...args] as const,
+    detail: (args: unknown[]) => [...keys.details(), ...args] as const,
   };
   return keys;
 };
