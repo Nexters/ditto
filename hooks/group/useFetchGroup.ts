9952eacf3f867b92ce35dfa28eded9fd7192de95
young-do <yd199323@gmail.com>
2023-02-27 18:39:12 +0900

    adjust group key

--- hooks/group/useFetchGroup.ts
+++ hooks/group/useFetchGroup.ts
@@ -1,9 +1,10 @@
 import { getGroup } from '@/lib/supabase/apis/group';
 import { useQuery } from '@tanstack/react-query';
+import { GROUP_KEY } from '../queries/keys';
 
 export const useFetchGroup = (group_id?: number | null) => {
   return useQuery(
-    ['useFetchGroup', group_id],
+    GROUP_KEY.detail([group_id]),
     async () => {
       if (!group_id) throw 'need to select group';
       return await getGroup(group_id);
--- hooks/group/useFetchJoinedGroupList.ts
+++ hooks/group/useFetchJoinedGroupList.ts
@@ -1,10 +1,11 @@
 import { getJoinedGroupList } from '@/lib/supabase/apis/group';
 import { User } from '@/lib/supabase/type';
 import { useQuery } from '@tanstack/react-query';
+import { GROUP_KEY } from '../queries/keys';
 
 export const useFetchJoinedGroupList = (user?: User | null) => {
   return useQuery(
-    ['groups', user],
+    GROUP_KEY.list([user]),
     async () => {
       if (!user) throw 'need to login';
       return await getJoinedGroupList(user.id);
