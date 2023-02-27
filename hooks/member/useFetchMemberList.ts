fe6e635f08b11226b4bf72d27b95b54b5ef97a2d
young-do <yd199323@gmail.com>
2023-02-27 18:33:10 +0900

    adjust member key

--- hooks/member/useFetchMemberList.ts
+++ hooks/member/useFetchMemberList.ts
@@ -1,10 +1,11 @@
 import { getMemberListByGroupId } from '@/lib/supabase/apis/member';
 import { User } from '@/lib/supabase/type';
 import { useQuery } from '@tanstack/react-query';
+import { MEMBER_KEY } from '../queries/keys';
 
 export const useFetchMemberList = (user?: User | null, group_id?: number | null) => {
   return useQuery(
-    ['member_list', user, group_id],
+    MEMBER_KEY.list([user, group_id]),
     async () => {
       if (!user) throw 'need to login';
       if (!group_id) throw 'need to select group';
