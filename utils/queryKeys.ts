cde97e479cc5a98049646b058dce50a2b8dfae9f
young-do <yd199323@gmail.com>
2023-02-27 18:28:21 +0900

    remove generic

--- utils/queryKeys.ts
+++ utils/queryKeys.ts
@@ -1,11 +1,11 @@
 // ref: https://tkdodo.eu/blog/effective-react-query-keys
-const createQueryKeys = <Filters = unknown, Detail = unknown>(keyword: string) => {
+const createQueryKeys = (keyword: string) => {
   const keys = {
     all: [keyword] as const,
     lists: () => [...keys.all, 'list'] as const,
-    list: (filters: Filters) => [...keys.lists(), { filters }] as const,
+    list: (...args: any[]) => [...keys.lists(), ...args] as const,
     details: () => [...keys.all, 'detail'] as const,
-    detail: (id: Detail) => [...keys.details(), id] as const,
+    detail: (...args: any[]) => [...keys.details(), ...args] as const,
   };
   return keys;
 };
