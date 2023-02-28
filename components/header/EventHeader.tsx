71986de6220ca9186fa1f3602bfa012d0c8d7035
young-do <yd199323@gmail.com>
2023-02-28 14:59:27 +0900

    refactor header

--- components/header/EventHeader.tsx
+++ components/header/EventHeader.tsx
@@ -1,20 +1,9 @@
 import React from 'react';
-import GroupMenu from '../groupMenu/GroupMenu';
 import { CalendarIcon } from '../icons';
 import CommonHeader from './CommonHeader';
 
 const EventHeader = () => {
-  return (
-    <CommonHeader>
-      <CommonHeader.Left>
-        <GroupMenu />
-        <h1>일정</h1>
-      </CommonHeader.Left>
-      <CommonHeader.Right>
-        <CalendarIcon />
-      </CommonHeader.Right>
-    </CommonHeader>
-  );
+  return <CommonHeader title="일정" icon={<CalendarIcon />} />;
 };
 
 export default EventHeader;
