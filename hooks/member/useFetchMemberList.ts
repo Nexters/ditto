cfe0d0d5456d060d2367e18a735397b60ef39eef
Young-do Cho <yd199323@gmail.com>
2023-03-02 01:47:42 +0900

    fix: useFetchMemberList에서 나 자신 포함되도록 수정 (#128)

--- hooks/member/useFetchMemberList.ts
+++ hooks/member/useFetchMemberList.ts
@@ -1,16 +1,14 @@
 import { getMemberListByGroupId } from '@/lib/supabase/apis/member';
-import { User } from '@/lib/supabase/type';
 import { MEMBER_KEY } from '@/utils/const';
 import { useQuery } from '@tanstack/react-query';
 
-export const useFetchMemberList = (user?: User | null, group_id?: number | null) => {
+export const useFetchMemberList = (group_id?: number | null) => {
   return useQuery(
-    MEMBER_KEY.list([user, group_id]),
+    MEMBER_KEY.list([group_id]),
     async () => {
-      if (!user) throw 'need to login';
       if (!group_id) throw 'need to select group';
-      return await getMemberListByGroupId(user.id, group_id);
+      return await getMemberListByGroupId(group_id);
     },
-    { enabled: !!user && !!group_id }
+    { enabled: !!group_id }
   );
 };
