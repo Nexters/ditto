d3f69b0f1dc8f11ad18a52162808bcafb3a54512
sian.me <sian.me@kakaoenterprise.com>
2023-03-02 00:10:59 +0900

    fix: modal title, creator 정보 추가

--- components/modals/bucketList/UpdateBucketItemModal.tsx
+++ components/modals/bucketList/UpdateBucketItemModal.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Box, Divider, ModalBody, ModalFooter, ModalHeader, Text } from '@chakra-ui/react';
 import BaseModal from '@/components/modals/BaseModal';
 import TitleTextarea from '@/components/inputs/TitleTextarea';
@@ -8,23 +8,30 @@ import BaseButton from '@/components/buttons/BaseButton';
 import { useMutateBucketItems } from '@/hooks/bucketlist/useMutateBucketItems';
 import { TrashCanIcon } from '@/components/icons';
 import theme from '@/styles/theme';
+import PartialLoader from '@/components/loading/PartialLoader';
+import { useFetchBucketFolderById } from '@/hooks/bucketlist/useFetchBucketFolderById';
+import { TBucketItem } from '@/lib/supabase/type';
+import { formatCreationDate } from '@/utils/date';
+import useChangeCreatorIdToName from '@/hooks/bucketlist/useChangeCreatorIdToName';
 
 interface UpdateBucketItemModalProps {
   isOpen: boolean;
   onClose: () => void;
-  title: string;
-  description: string | null;
-  id: number;
+  bucketItem: TBucketItem;
 }
 
-const UpdateBucketItemModal = ({ isOpen, onClose, description, title, id }: UpdateBucketItemModalProps) => {
+const UpdateBucketItemModal = ({ isOpen, onClose, bucketItem }: UpdateBucketItemModalProps) => {
+  const { title, description, id, bucket_folder_id: folderId, created_time, creator_id } = bucketItem;
   const ModalContent = () => {
-    const [itemTitle, setItemTitle] = React.useState(title);
-    const [itemDesc, setItemDesc] = React.useState(description);
+    const [itemTitle, setItemTitle] = useState(title);
+    const [itemDesc, setItemDesc] = useState(description);
 
     const { updateBucketItemMutation, deleteBucketItemMutation } = useMutateBucketItems();
     const { mutate: updateBucketItem } = updateBucketItemMutation;
     const { mutate: deleteBucketItem } = deleteBucketItemMutation;
+
+    const { data: folder, isLoading } = useFetchBucketFolderById(folderId);
+    const { changeCreatorIdToName } = useChangeCreatorIdToName();
 
     const handleClickEditButton = () => {
       updateBucketItem(
@@ -54,7 +61,10 @@ const UpdateBucketItemModal = ({ isOpen, onClose, description, title, id }: Upda
     return (
       <>
         <StyledModalHeader>
-          <StyledModalHeaderText textStyle={'body1'}>Modal header</StyledModalHeaderText>
+          <StyledModalHeaderText textStyle={'body1'}>
+            {isLoading && <PartialLoader />}
+            {folder?.title || ''}
+          </StyledModalHeaderText>
         </StyledModalHeader>
         <ModalBody padding={0}>
           <TitleTextarea
@@ -69,13 +79,17 @@ const UpdateBucketItemModal = ({ isOpen, onClose, description, title, id }: Upda
           <Box padding={'20px'}>
             <ContentTextarea
               placeholder={'설명을 입력하세요'}
-              height={94}
+              height={73}
               value={itemDesc ?? ''}
               onChange={(e) => {
                 setItemDesc(e.target.value);
               }}
             />
           </Box>
+          <CreatorText>
+            <span>{formatCreationDate(created_time)}</span>
+            <span>{changeCreatorIdToName(creator_id)}</span>
+          </CreatorText>
         </ModalBody>
         <ModalFooter justifyContent={'space-between'}>
           <TrashCanIcon cursor="pointer" onClick={handleClickDeleteButton} />
@@ -101,6 +115,15 @@ const StyledModalHeaderText = styled(Text)`
   padding-top: 4px;
   padding-bottom: 4px;
   text-align: center;
+`;
+
+const CreatorText = styled(Text)`
+  display: flex;
+  flex-direction: column;
+  color: ${theme.colors.grey[4]};
+  ${theme.textStyles.body3};
+  margin-left: 20px;
+  gap: 6px;
 `;
 
 export default UpdateBucketItemModal;
