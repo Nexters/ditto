100cc10061461fcc91640dcd3aad06cbe1256b29
sangbooom <tkdqja4164@naver.com>
2023-03-01 17:06:20 +0900

    refactor: 일정 쿼리키 정리

--- hooks/Event/useUpdateEvent.ts
+++ hooks/Event/useUpdateEvent.ts
@@ -5,10 +5,11 @@ import { EVENT_KEY } from '@/utils/const';
 
 export const useUpdateEvent = (options?: Omit<UseMutationOptions<void, Error, UpdateEventType>, 'mutationFn'>) => {
   const queryClient = useQueryClient();
+
   return useMutation(async (params: UpdateEventType) => await updateEvent(params), {
     ...options,
     onSuccess: (data, variables, context) => {
-      queryClient.invalidateQueries(EVENT_KEY.all);
+      queryClient.invalidateQueries({ queryKey: EVENT_KEY.lists() });
       options?.onSuccess?.(data, variables, context);
       // TODO: 일정 수정 완료 토스트 띄우기
     },
