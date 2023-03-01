67215369fec4f40f7070d14a079e74eef69941f9
sangbooom <tkdqja4164@naver.com>
2023-03-01 17:28:53 +0900

    fix: lint-staged 변경

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,13 +1,10 @@
 module.exports = {
   // Type check TypeScript files
-  '**/*.(ts|tsx)': () => 'yarn tsc --noEmit',
+  '**/*.(ts|tsx)': () => 'tsc --noEmit',
 
   // Lint then format TypeScript and JavaScript files
-  '**/*.(ts|tsx|js)': (filenames) => [
-    `yarn eslint --fix ${filenames.join(' ')}`,
-    `yarn prettier --write ${filenames.join(' ')}`,
-  ],
+  '**/*.(ts|tsx|js)': (filenames) => [`eslint --fix ${filenames.join(' ')}`, `prettier --write ${filenames.join(' ')}`],
 
   // Format MarkDown and JSON
-  '**/*.(md|json)': (filenames) => `yarn prettier --write ${filenames.join(' ')}`,
+  '**/*.(md|json)': (filenames) => `prettier --write ${filenames.join(' ')}`,
 };
