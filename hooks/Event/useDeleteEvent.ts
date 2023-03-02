f8bb9181ee93b0633120f8a3a27f4252933e24b5
sangbooom <tkdqja4164@naver.com>
2023-03-02 23:23:15 +0900

    feat: 일정 생성, 수정, 삭제시 토스트 띄우기

--- hooks/Event/useDeleteEvent.ts
+++ hooks/Event/useDeleteEvent.ts
@@ -1,19 +1,21 @@
 import { deleteEvent } from '@/lib/supabase/apis/event';
 import { EVENT_KEY } from '@/utils/const';
 import { useQueryClient, useMutation, UseMutationOptions } from '@tanstack/react-query';
+import useCustomToast from '../shared/useCustomToast';
 
 export const useDeleteEvent = (options?: Omit<UseMutationOptions<void, Error, number>, 'mutationFn'>) => {
   const queryClient = useQueryClient();
+  const { openToast } = useCustomToast();
 
   return useMutation(async (eventId: number) => await deleteEvent(eventId), {
     ...options,
     onSuccess: (data, variables, context) => {
       queryClient.invalidateQueries({ queryKey: EVENT_KEY.lists() });
       options?.onSuccess?.(data, variables, context);
-      // TODO: 삭제 완료 토스트 띄우기
+      openToast({ message: '일정이 삭제되었습니다.', type: 'success' });
     },
     onError: () => {
-      // TODO: 삭제 에러 토스트 띄우기
+      openToast({ message: '일정 삭제에 실패했습니다. 화면을 새로고침해주세요', type: 'error' });
     },
   });
 };
--- hooks/Event/useUpdateEvent.ts
+++ hooks/Event/useUpdateEvent.ts
@@ -2,19 +2,21 @@ import { updateEvent } from '@/lib/supabase/apis/event';
 import { useQueryClient, useMutation, UseMutationOptions } from '@tanstack/react-query';
 import { UpdateEventType } from '@/lib/supabase/apis/event/type';
 import { EVENT_KEY } from '@/utils/const';
+import useCustomToast from '../shared/useCustomToast';
 
 export const useUpdateEvent = (options?: Omit<UseMutationOptions<void, Error, UpdateEventType>, 'mutationFn'>) => {
   const queryClient = useQueryClient();
+  const { openToast } = useCustomToast();
 
   return useMutation(async (params: UpdateEventType) => await updateEvent(params), {
     ...options,
     onSuccess: (data, variables, context) => {
       queryClient.invalidateQueries({ queryKey: EVENT_KEY.lists() });
       options?.onSuccess?.(data, variables, context);
-      // TODO: 일정 수정 완료 토스트 띄우기
+      openToast({ message: '일정이 편집되었습니다.', type: 'success' });
     },
     onError: () => {
-      // TODO: 일정 수정 에러 토스트 띄우기
+      openToast({ message: '일정 편집에 실패했습니다. 화면을 새로고침해주세요', type: 'success' });
     },
   });
 };
