4eaea0ff8e065f84b3dbf4533bf9a112e8882a93
young-do <yd199323@gmail.com>
2023-03-02 13:07:43 +0900

    fix

--- hooks/bucketlist/useChangeCreatorIdToName.ts
+++ hooks/bucketlist/useChangeCreatorIdToName.ts
@@ -2,12 +2,12 @@ import { useUser } from '@/store/useUser';
 import { useFetchMemberList } from '@/hooks/member/useFetchMemberList';
 
 const useChangeCreatorIdToName = () => {
-  const { user, selectedGroupId } = useUser();
-  const { data: userList } = useFetchMemberList(user, selectedGroupId);
+  const { selectedGroupId } = useUser();
+  const { data: memberList } = useFetchMemberList(selectedGroupId);
 
   const changeCreatorIdToName = (creatorId: number) => {
-    if (!userList) return;
-    const creator = userList.find((user) => user.id === creatorId);
+    if (!memberList) return;
+    const creator = memberList.find((user) => user.id === creatorId);
     return creator?.nickname ?? '알 수 없음';
   };
 
