e228df40d23d37d3166324c36eead71dd9077ae1
Young-do Cho <yd199323@gmail.com>
2023-03-02 13:12:54 +0900

    hotfix: 빌드 에러 실패 수정 (#132)
    * fix type error
    
    * change to return only string

--- hooks/bucketlist/useChangeCreatorIdToName.ts
+++ hooks/bucketlist/useChangeCreatorIdToName.ts
@@ -2,12 +2,11 @@ import { useUser } from '@/store/useUser';
 import { useFetchMemberList } from '@/hooks/member/useFetchMemberList';
 
 const useChangeCreatorIdToName = () => {
-  const { user, selectedGroupId } = useUser();
-  const { data: userList } = useFetchMemberList(user, selectedGroupId);
+  const { selectedGroupId } = useUser();
+  const { data: memberList } = useFetchMemberList(selectedGroupId);
 
   const changeCreatorIdToName = (creatorId: number) => {
-    if (!userList) return;
-    const creator = userList.find((user) => user.id === creatorId);
+    const creator = memberList?.find((user) => user.id === creatorId);
     return creator?.nickname ?? '알 수 없음';
   };
 
