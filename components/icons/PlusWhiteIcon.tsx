76f902feba7f62378a659f0428fddceeb72d795e
sian.me <sian.me@kakaoenterprise.com>
2023-03-03 16:53:27 +0900

    fix: add folder icon 변경

--- components/icons/PlusWhiteIcon.tsx
+++ components/icons/PlusWhiteIcon.tsx
@@ -1,8 +1,16 @@
 import * as React from 'react';
 import { SVGProps } from 'react';
-const SvgPlusWhiteIcon = (props: SVGProps<SVGSVGElement>) => (
-  <svg width={32} height={32} viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
-    <path d="M5 16h22M16 5v22" stroke="#fff" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" />
-  </svg>
-);
-export default SvgPlusWhiteIcon;
+
+type CustomSvgProps = SVGProps<SVGSVGElement> & {
+  strokeColor?: string;
+};
+
+const PlusWhiteIcon = (props: CustomSvgProps) => {
+  const { strokeColor = '#fff' } = props;
+  return (
+    <svg width={32} height={32} viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+      <path d="M5 16h22M16 5v22" stroke={strokeColor} strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" />
+    </svg>
+  );
+};
+export default PlusWhiteIcon;
