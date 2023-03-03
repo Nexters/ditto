9bcd7502a4729707264242bf6517cca88a8ff2ad
sian.me <sian.me@kakaoenterprise.com>
2023-03-03 17:06:47 +0900

    fix: props 이슈 수정

--- components/icons/PlusWhiteIcon.tsx
+++ components/icons/PlusWhiteIcon.tsx
@@ -5,8 +5,7 @@ type CustomSvgProps = SVGProps<SVGSVGElement> & {
   strokeColor?: string;
 };
 
-const PlusWhiteIcon = (props: CustomSvgProps) => {
-  const { strokeColor = '#fff' } = props;
+const PlusWhiteIcon = ({ strokeColor = '#fff', ...props }: CustomSvgProps) => {
   return (
     <svg width={32} height={32} viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
       <path d="M5 16h22M16 5v22" stroke={strokeColor} strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" />
