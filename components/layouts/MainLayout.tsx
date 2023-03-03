ff026245b7a2302c320580095f6dd84696d929c8
young-do <yd199323@gmail.com>
2023-03-03 18:20:41 +0900

    add bg color

--- components/layouts/MainLayout.tsx
+++ components/layouts/MainLayout.tsx
@@ -1,6 +1,7 @@
 import { ReactNode } from 'react';
 import styled from '@emotion/styled';
 import BottomNavigation, { BOTTOM_NAV_HEIGHT } from './BottomNavigation';
+import theme from '@/styles/theme';
 
 type MainLayoutProps = {
   children: ReactNode;
@@ -34,6 +35,7 @@ const MainContainer = styled.main`
   max-width: 420px;
   min-height: calc(var(--vh, 1vh) * 100);
   margin: 0 auto;
+  background-color: ${theme.colors.white};
 `;
 
 const MainSection = styled.section<{ headerHeight: number; bottomNavHeight: number }>`
