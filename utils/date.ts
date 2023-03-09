bcbb000747907dd65ea77fa4fe6b1ac8184bc3ac
sangbooom <tkdqja4164@naver.com>
2023-03-10 01:29:35 +0900

    refactor: date 9시간 빼기없이 계산하기

--- utils/date.ts
+++ utils/date.ts
@@ -5,15 +5,11 @@ export const eventDateForView = (isAllDay: boolean, date?: string) => {
   return isAllDay
     ? formatISO(date ? new Date(date) : new Date(), { representation: 'date' })
     : formatISO(date ? new Date(date) : new Date()).slice(0, -9);
-  // const KSTDate = date ? new Date(date) : new Date();
-  // const UTCDate = date ? utcToZonedTime(date, 'UTC') : new Date();
-  // const conditionalDate = mode === 'create' ? KSTDate : UTCDate;
-  // return isAllDay ? formatISO(conditionalDate, { representation: 'date' }) : formatISO(conditionalDate).slice(0, -9);
 };
 
 export const eventDateForSave = (isAllDay: boolean, date: string) => {
   return isAllDay
-    ? format(utcToZonedTime(date, 'UTC'), 'yyyy-MM-dd')
+    ? format(utcToZonedTime(`${date}Z`, 'UTC'), 'yyyy-MM-dd')
     : format(utcToZonedTime(date, 'UTC'), 'yyyy-MM-dd HH:mm');
 };
 
@@ -38,51 +34,31 @@ export const today = (startDate: string, endDate: string) => {
   return isToday(new Date(startDate)) || isToday(new Date(endDate));
 };
 
-////////// 여기가 바뀌어야됨
-
 // MM월 dd일
-export const dateChangeToMMdd = (date?: string) => {
+const formatToMMdd = (date?: string) => {
   const targetDate = date ? new Date(date) : new Date();
   return `${getMonth(targetDate) + 1}월 ${getDate(targetDate)}일`;
 };
 
 // hh:mm
-export const dateChangeToHHmm = (date?: string) => {
+const formatToHHmm = (date?: string) => {
   const targetDate = date ? new Date(date) : new Date();
   return format(targetDate, 'HH:mm');
 };
 
-const eventFormat = (targetDate: string) => `${dateChangeToMMdd(targetDate)} ${dateChangeToHHmm(targetDate)}`;
+const eventFormat = (targetDate: string) => `${formatToMMdd(targetDate)} ${formatToHHmm(targetDate)}`;
 
-export const dateChangeToEventFormat = (startDate: string, endDate: string) => {
+export const changedToEventDate = (isAllDay: boolean, startDate: string, endDate: string) => {
   const changedToMMdd = eventFormat(startDate);
   const changedToHHmm = eventFormat(endDate);
 
-  if (startDate === endDate) {
-    if (format(new Date(startDate), 'HH:mm') === '00:00') return changedToMMdd.slice(0, -5);
-    return changedToMMdd || changedToHHmm;
+  if (isAllDay && format(new Date(startDate), 'HH:mm') === format(new Date(endDate), 'HH:mm')) {
+    if (format(new Date(startDate), 'MM dd') === format(new Date(endDate), 'MM dd')) {
+      return changedToMMdd.slice(0, -5);
+    } else {
+      return `${changedToMMdd.slice(0, -5)} - ${changedToHHmm.slice(0, -5)}`;
+    }
   }
 
-  if ([format(new Date(startDate), 'HH:mm'), format(new Date(endDate), 'HH:mm')].every((v) => v === '00:00'))
-    return `${changedToMMdd.slice(0, -5)} - ${changedToHHmm.slice(0, -5)}`;
   return `${changedToMMdd} - ${changedToHHmm}`;
-};
-
-////////// 이렇게 바꾸자
-
-const formatMMddHHmm = (targetDate: string) =>
-  `${format(utcToZonedTime(targetDate, 'UTC'), 'MM월 dd일')} ${format(utcToZonedTime(targetDate, 'UTC'), 'HH:mm')}`;
-
-export const formatEventDate = (startDate: string, endDate: string) => {
-  const formattedStartDate = formatMMddHHmm(startDate);
-  const formattedEndDate = formatMMddHHmm(endDate);
-
-  // if (startDate === endDate) {
-  //   if (format(new Date(startDate), 'HH:mm') === '00:00') return formattedStartDate.slice(0, -5);
-  //   return formattedStartDate || formattedEndDate;
-  // }
-
-  // if ([format(new Date(startDate), 'HH:mm'), format(new Date(endDate), 'HH:mm')].every((v) => v === '00:00'))
-  //   return `${formattedStartDate.slice(0, -5)} - ${formattedEndDate.slice(0, -5)}`;
-  return `${formattedStartDate} - ${formattedEndDate}`;
 };
