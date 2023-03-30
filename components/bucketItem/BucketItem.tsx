eb17c2c5a3d571e92cdad50c8115fd2c9d78a6bf
young-do <yd199323@gmail.com>
2023-03-30 10:29:11 +0900

    버킷리스트 완료했을때 토스트 띄우기

--- components/bucketItem/BucketItem.tsx
+++ components/bucketItem/BucketItem.tsx
@@ -5,18 +5,29 @@ import PawButton from '@/components/buttons/PawButton';
 import styled from '@emotion/styled';
 import { useMutateBucketItems } from '@/hooks/bucketlist/useMutateBucketItems';
 import theme from '@/styles/theme';
+import useCustomToast from '@/hooks/shared/useCustomToast';
 
 const BucketItem = ({ item }: { item: TBucketItem }) => {
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const { openToast } = useCustomToast();
 
   const { completeBucketItemMutation } = useMutateBucketItems();
   const { mutate: completeBucketItem } = completeBucketItemMutation;
 
   const handleClickPawButton = () => {
-    completeBucketItem({
-      id: item.id,
-      completed: !item.completed,
-    });
+    completeBucketItem(
+      {
+        id: item.id,
+        completed: !item.completed,
+      },
+      {
+        onSuccess: () => {
+          if (!item.completed) {
+            openToast({ message: '버킷리스트 완료!', type: 'success' });
+          }
+        },
+      }
+    );
   };
 
   return (
