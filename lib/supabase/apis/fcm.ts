5550a110c63653041384fce70a165bf936e4de3c
young-do <yd199323@gmail.com>
2023-05-19 09:01:57 +0900

    잘못된 주석 제거

--- lib/supabase/apis/fcm.ts
+++ lib/supabase/apis/fcm.ts
@@ -2,9 +2,10 @@ import { supabase } from '../client';
 
 export const saveToken = async (user_id: number, token: string) => {
   // @note: upsert는 존재하지 않으면 추가하고, 존재하면 업데이트를 한다.
-  // ignoreDuplicates = false를 설정한 이유는 존재하는 경우 아무것도 안하게 하고자 함이다.
+  // link: https://supabase.com/docs/reference/javascript/upsert
   const { error } = await supabase
     .from('fcm_tokens')
     .upsert({ user_id, token, last_used_time: new Date().toISOString() });
+
   if (error) throw error;
 };
