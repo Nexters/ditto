bae6e2606b6d6850a38b9255d775101a915cd585
Young-do Cho <yd199323@gmail.com>
2023-05-22 00:33:57 +0900

    feat: web push 메시지 노출 개선 (#154)
    * push 알림 클릭했을 때 특정 url로 이동되도록
    
    * body 의 각 필드에 대한 설명 추가

--- pages/api/fcm/send-message.ts
+++ pages/api/fcm/send-message.ts
@@ -9,10 +9,18 @@ export const config = {
 };
 
 const bodyScheme = z.object({
+  /** 보내는 유저 id. push 메시지 보낼 때 이 유저는 제외된다. */
   sender_id: z.number(),
+  /** push 메시지 보낼 그룹 id */
   group_id: z.number(),
+  /** push 메시지 제목 */
   notification_title: z.string(),
+  /** push 메시지 내용 */
   notification_body: z.string(),
+  /** push 메시지 아이콘 */
+  notification_icon: z.string(),
+  /** push 메시지 클릭 시 이동할 url */
+  notification_click_action: z.string(),
 });
 
 const edgeFunction: EdgeFunction = async (req) => {
@@ -23,7 +31,15 @@ const edgeFunction: EdgeFunction = async (req) => {
     if (!refreshToken || !accessToken) throw 'no authorized';
 
     const body = await req.json();
-    const { sender_id, group_id, notification_title, notification_body } = bodyScheme.parse(body);
+    const {
+      sender_id,
+      group_id,
+      notification_title,
+      notification_body,
+      notification_icon,
+      notification_click_action,
+      //
+    } = bodyScheme.parse(body);
 
     // 1. 푸시 알림을 보내기 위한 그룹 내 다른 멤버 token들을 가져온다
     // 2. firebase http(legacy, not v1) 호출을 통해 푸시 알림을 보낸다
@@ -43,6 +59,8 @@ const edgeFunction: EdgeFunction = async (req) => {
             notification: {
               title: notification_title,
               body: notification_body,
+              icon: notification_icon,
+              click_action: notification_click_action,
             },
           }),
         });
