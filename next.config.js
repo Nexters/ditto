1ba5cefc557115cca18f88110f2eab9dbcc03ab3
Young-do Cho <yd199323@gmail.com>
2023-06-23 14:31:08 +0900

    feat: sentry 연동 (#156)
    * sentry 설정
    
    * ie11까지 지원하는 건 불필요해서 제거

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
 const withPWA = require('next-pwa')({ dest: 'public' });
+const { withSentryConfig } = require('@sentry/nextjs');
 
 const nextConfig = {
   reactStrictMode: true,
@@ -26,4 +27,37 @@ const nextConfig = {
   },
 };
 
-module.exports = withPWA(nextConfig);
+module.exports = withSentryConfig(
+  withPWA(nextConfig),
+
+  {
+    // For all available options, see:
+    // https://github.com/getsentry/sentry-webpack-plugin#options
+
+    // Suppresses source map uploading logs during build
+    silent: true,
+
+    org: 'racoon-test',
+    project: 'ditto-nextjs',
+  },
+
+  {
+    // For all available options, see:
+    // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+
+    // Upload a larger set of source maps for prettier stack traces (increases build time)
+    widenClientFileUpload: true,
+
+    // Transpiles SDK to be compatible with IE11 (increases bundle size)
+    transpileClientSDK: false,
+
+    // Routes browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers (increases server load)
+    tunnelRoute: '/monitoring',
+
+    // Hides source maps from generated client bundles
+    hideSourceMaps: true,
+
+    // Automatically tree-shake Sentry logger statements to reduce bundle size
+    disableLogger: true,
+  }
+);
