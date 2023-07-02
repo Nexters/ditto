7d864486c31f616cd771920356392d748738f3d3
young-do <yd199323@gmail.com>
2023-07-02 17:02:33 +0900

    폴더 페이지에서 나갈때 읽음처리 되도록

--- components/bucketItem/BucketItemList.tsx
+++ components/bucketItem/BucketItemList.tsx
@@ -1,4 +1,4 @@
-import React, { memo } from 'react';
+import React, { memo, useEffect } from 'react';
 import { useRouter } from 'next/router';
 import { useFetchBucketItems } from '@/hooks/bucketlist/useFetchBucketItems';
 import BucketItem from '@/components/bucketItem/BucketItem';
@@ -6,16 +6,25 @@ import styled from '@emotion/styled';
 import PartialLoader from '@/components/loading/PartialLoader';
 import EmptyItem from '@/components/bucketItem/EmptyItem';
 import BucketItemInput from '@/components/bucketItem/BucketItemInput';
+import { useUnreadBucketItems } from '@/hooks/bucketlist/useUnreadBucketItems';
 
 const BucketItemList = () => {
   const router = useRouter();
-  const { folderId } = router.query;
+  const folderId = Number(router.query.folderId ?? '');
 
-  const { data: bucketItems = [], isLoading } = useFetchBucketItems(Number(folderId));
+  const { setReadByFolderId } = useUnreadBucketItems();
+  const { data: bucketItems = [], isLoading } = useFetchBucketItems(folderId);
+
+  useEffect(() => {
+    // @note: 이 페이지로 넘어오면 각 아이템들을 읽음 처리한다
+    const handleRouteChange = () => folderId && setReadByFolderId(folderId);
+    router.events.on('routeChangeStart', handleRouteChange);
+    return () => router.events.off('routeChangeStart', handleRouteChange);
+  }, [folderId, router.events, setReadByFolderId]);
 
   return (
     <>
-      <BucketItemInput folderId={Number(folderId ?? '')} />
+      <BucketItemInput folderId={folderId} />
       <ListWrapper>
         {isLoading ? (
           <PartialLoader />
--- hooks/bucketlist/useUnreadBucketItems.ts
+++ hooks/bucketlist/useUnreadBucketItems.ts
@@ -1,12 +1,15 @@
 import { getUnreadBucketItems } from '@/lib/supabase/apis/bucketlist';
+import { TBucketItem } from '@/lib/supabase/type';
 import { useUser } from '@/store/useUser';
 import { BUCKET_ITEM_KEY } from '@/utils/const';
-import { useQuery } from '@tanstack/react-query';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
+import { useCallback } from 'react';
 
 export const useUnreadBucketItems = () => {
+  const queryClient = useQueryClient();
   const { user, selectedGroupId } = useUser();
 
-  return useQuery(
+  const query = useQuery(
     BUCKET_ITEM_KEY.list(['unread', selectedGroupId]),
     async () => {
       if (!user) throw 'no authorized';
@@ -19,4 +22,16 @@ export const useUnreadBucketItems = () => {
       enabled: !!selectedGroupId && !!user,
     }
   );
+
+  const setReadByFolderId = useCallback(
+    (folderId: number) => {
+      queryClient.setQueryData<Pick<TBucketItem, 'id' | 'bucket_folder_id'>[]>(
+        BUCKET_ITEM_KEY.list(['unread', selectedGroupId]),
+        (prev) => prev?.filter((item) => item.bucket_folder_id !== folderId) ?? []
+      );
+    },
+    [queryClient, selectedGroupId]
+  );
+
+  return { ...query, setReadByFolderId };
 };
